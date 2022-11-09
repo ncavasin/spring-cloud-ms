da7f11772f9abb4c45fb56d3cf5042bd9daea779
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-08 23:30:52 -0300

    implement customer module

--- customer/src/main/java/com/ms/customer/CustomerApplication.java
+++ customer/src/main/java/com/ms/customer/CustomerApplication.java
@@ -2,6 +2,7 @@ package com.ms.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class CustomerApplication {
