4c5c25a53e698412c83d4bac374a48e5804a4a9b
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-10 09:01:52 -0300

    implement conversion for getAll

--- customer/src/main/java/com/ms/customer/dto/CustomerConverter.java
+++ customer/src/main/java/com/ms/customer/dto/CustomerConverter.java
@@ -2,9 +2,18 @@ package com.ms.customer.dto;
 
 import com.ms.customer.Customer;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CustomerConverter {
 
     public static CustomerDto convert(Customer customer) {
-        return new CustomerDto(customer.getEmail(), customer.getPassword());
+        return new CustomerDto(customer.getId(), customer.getEmail(), customer.getPassword());
+    }
+
+    public static List<CustomerDto> convert(List<Customer> customers) {
+        return customers.stream()
+                .map(CustomerConverter::convert)
+                .collect(Collectors.toList());
     }
 }
