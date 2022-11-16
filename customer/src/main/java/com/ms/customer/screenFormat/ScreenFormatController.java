db761c6463f1cee60877d22e483de4d770efe142
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-15 22:32:43 -0300

    add "/all" to ScreenFormatController endpoint

--- customer/src/main/java/com/ms/customer/screenFormat/ScreenFormatController.java
+++ customer/src/main/java/com/ms/customer/screenFormat/ScreenFormatController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequestMapping("/api/format")
 public record ScreenFormatController(ScreenFormatService screenFormatService) {
 
-    @GetMapping
+    @GetMapping("/all")
     public List<ScreenFormatDto> getAll() {
         return ScreenFormatConverter.convert(this.screenFormatService.getAll());
     }
