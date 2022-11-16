21ed959443d69091f7294c5e5ff2f919a1efd710
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-16 00:34:57 -0300

    add tests runner case

---
+++ customer/src/test/java/com/ms/customer/CustomerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ms.customer;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+//@ActiveProfiles("mem")
+@SpringBootTest
+public class CustomerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
