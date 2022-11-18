4174a6e905546389f923567e205324baa872d153
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-18 16:59:41 -0300

    add logger bean

--- customer/src/main/java/com/ms/customer/CustomerApplication.java
+++ customer/src/main/java/com/ms/customer/CustomerApplication.java
@@ -1,10 +1,19 @@
 package com.ms.customer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerApplication {
+
+    @Bean
+    public Logger logger(){
+        return LoggerFactory.getLogger("Default logger");
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
