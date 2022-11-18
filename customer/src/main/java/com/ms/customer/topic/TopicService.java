b7a0d158a9b4a4b8dac3fa87e407dce8941bc30a
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-18 17:18:39 -0300

    add logging to TopicService

--- customer/src/main/java/com/ms/customer/topic/TopicService.java
+++ customer/src/main/java/com/ms/customer/topic/TopicService.java
@@ -2,39 +2,47 @@ package com.ms.customer.topic;
 
 import com.ms.customer.shared.exceptions.BadRequest;
 import com.ms.customer.shared.exceptions.NotFound;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public record TopicService(TopicRepository topicRepository) {
+public record TopicService(TopicRepository topicRepository, Logger logger) {
     public List<Topic> findAll() {
         return this.topicRepository.findAll();
     }
 
     public Topic findById(String topicId) {
         return this.topicRepository.findById(topicId)
-                .orElseThrow(() -> new NotFound(String.format("Topic with id %s not found.", topicId)));
+                .orElseThrow(() -> {
+                    logger.warn("Topic with id '{}' not found.", topicId);
+                    throw new NotFound(String.format("Topic with id %s not found.", topicId));
+                });
     }
 
     public Topic add(Topic topic) {
+        logger.info("Topic with name '{}' created.", topic.getName());
         return this.topicRepository.save(topic);
     }
 
     public Topic update(String topicId, Topic topic) {
         existsById(topicId);
         topic.setId(topicId);
+        logger.info("Topic with id '{}' updated.", topic.getId());
         return this.topicRepository.save(topic);
-    }
-
-    private void existsById(String topicId) {
-        if (!this.topicRepository.existsById(topicId)) {
-            throw new BadRequest(String.format("Topic with id %s does not exists", topicId));
-        }
     }
 
     public void delete(String topicId) {
         existsById(topicId);
+        logger.info("Topic with id '{}' deleted.", topicId);
         this.topicRepository.deleteById(topicId);
+    }
+
+    private void existsById(String topicId) {
+        if (!this.topicRepository.existsById(topicId)) {
+            logger.warn("Topic with id '{}' does not exsit.", topicId);
+            throw new BadRequest(String.format("Topic with id %s does not exists", topicId));
+        }
     }
 }
