d93efa37846ff8867a94421314a8e74d6f6b603b
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-18 16:59:35 -0300

    add logging to customer service

--- customer/src/main/java/com/ms/customer/customer/CustomerService.java
+++ customer/src/main/java/com/ms/customer/customer/CustomerService.java
@@ -2,23 +2,30 @@ package com.ms.customer.customer;
 
 import com.ms.customer.shared.exceptions.BadRequest;
 import com.ms.customer.shared.exceptions.NotFound;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, Logger logger) {
     public List<Customer> getAll() {
         return this.customerRepository.findAll();
     }
 
     public Customer getById(String customerId) {
         return this.customerRepository.findById(customerId)
-                .orElseThrow(() -> new NotFound(String.format("Customer with id %s not found!", customerId)));
+                .orElseThrow(() -> {
+                    logger.info("Customer with id '{}' not found", customerId);
+                    throw new NotFound(String.format("Customer with id %s not found!", customerId));
+                });
     }
 
     public Customer add(Customer customer) {
         emailIsTaken(customer.getEmail());
+        log.info("New customer with email '{}' created.", customer.email);
         // TODO: validate email format
         return customerRepository.save(customer);
     }
@@ -27,6 +34,7 @@ public record CustomerService(CustomerRepository customerRepository) {
         validateCustomerExists(customerId);
         emailIsTaken(customer.getEmail());
         customer.setId(customerId);
+        log.info("Customer with id '{}' updated to: '{}'.", customerId, customer);
         // TODO: hash password
         return this.customerRepository.save(customer);
     }
@@ -35,17 +43,20 @@ public record CustomerService(CustomerRepository customerRepository) {
         if (!this.customerRepository.existsById(customerId)) {
             throw new NotFound(String.format("Customer with id %s not found!", customerId));
         }
+        this.logger.info("Customer with id '{}' deleted.", customerId);
         this.customerRepository.deleteById(customerId);
     }
 
     private void emailIsTaken(String email) {
         if (this.customerRepository.existsByEmail(email)) {
+            log.info("Email '{}' already taken", email);
             throw new BadRequest(String.format("Email %s already taken!", email));
         }
     }
 
     private void validateCustomerExists(String customerId) {
         if (!this.customerRepository.existsById(customerId)) {
+            log.info("Customer with id '{}' does not exist", customerId);
             throw new BadRequest(String.format("Customer with id %s does not exist", customerId));
         }
     }
