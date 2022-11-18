dff25ae9c2d2a648bd8bf3e400329645cda86582
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-18 17:12:12 -0300

    add logging to movie service

--- customer/src/main/java/com/ms/customer/movie/MovieService.java
+++ customer/src/main/java/com/ms/customer/movie/MovieService.java
@@ -3,13 +3,14 @@ package com.ms.customer.movie;
 import com.ms.customer.shared.exceptions.BadRequest;
 import com.ms.customer.shared.exceptions.NotFound;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @Slf4j
-public record MovieService(MovieRepository movieRepository) {
+public record MovieService(MovieRepository movieRepository, Logger logger) {
 
     public List<Movie> getAll() {
         return this.movieRepository.findAll();
@@ -17,10 +18,14 @@ public record MovieService(MovieRepository movieRepository) {
 
     public Movie getById(String movieId) {
         return this.movieRepository.findById(movieId)
-                .orElseThrow(() -> new NotFound(String.format("Movie with id %s not found.", movieId)));
+                .orElseThrow(() -> {
+                    logger.warn("Movie with id '{}' does not exist.", movieId);
+                    throw new NotFound(String.format("Movie with id %s not found.", movieId));
+                });
     }
 
     public Movie addMovie(Movie movie) {
+        logger.info("Movie with title '{}' created.", movie.getTitle());
         return this.movieRepository.save(movie);
     }
 
@@ -32,12 +37,15 @@ public record MovieService(MovieRepository movieRepository) {
         movie.setSynopsis(movieDto.synopsis);
         movie.setDuration(movie.duration);
         movie.setClassification(movieDto.getClassification());
+        logger.info("Movie with id '{}' updated.", movieId);
         return this.movieRepository.save(movie);
     }
 
     public void deleteMovie(String movieId) {
-        if (!this.movieRepository.existsById(movieId))
+        if (!this.movieRepository.existsById(movieId)) {
+            logger.warn("Could not delete Movie with id '{}'. It does not exist.", movieId);
             throw new BadRequest(String.format("Movie with id %s does not exist.", movieId));
+        }
         this.movieRepository.deleteById(movieId);
     }
 }
