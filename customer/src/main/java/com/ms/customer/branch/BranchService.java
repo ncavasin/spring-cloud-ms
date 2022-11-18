32a404c583a0b24e0a974bad511cb276fa1dc2df
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-18 17:09:49 -0300

    add logging to branch service

--- customer/src/main/java/com/ms/customer/branch/BranchService.java
+++ customer/src/main/java/com/ms/customer/branch/BranchService.java
@@ -2,12 +2,13 @@ package com.ms.customer.branch;
 
 import com.ms.customer.shared.exceptions.BadRequest;
 import com.ms.customer.shared.exceptions.NotFound;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public record BranchService(BranchRepository branchRepository) {
+public record BranchService(BranchRepository branchRepository, Logger logger) {
 
     public List<Branch> getAll() {
         return this.branchRepository.findAll();
@@ -15,18 +16,26 @@ public record BranchService(BranchRepository branchRepository) {
 
     public Branch getById(String branchId) {
         return this.branchRepository.findById(branchId)
-                .orElseThrow(() -> new NotFound(String.format("Branch with id %s not found.", branchId)));
+                .orElseThrow(() -> {
+                    logger.warn("Branch with id '{}' not found.", branchId);
+                    throw new NotFound(String.format("Branch with id %s not found.", branchId));
+                });
     }
 
     public Branch getByName(String name) {
         return this.branchRepository().findByName(name)
-                .orElseThrow(() -> new NotFound(String.format("Branch with name %s not found.", name)));
+                .orElseThrow(() -> {
+                    logger.warn("Branch with name '{}' not found", name);
+                    throw new NotFound(String.format("Branch with name %s not found.", name));
+                });
     }
 
     public Branch add(Branch branch) {
         if (this.branchRepository.existsByName(branch.getName())) {
-            throw new BadRequest(String.format("Branch with name %s already exists", branch.getName()));
+            logger.warn("Could not add Branch with name '{}'. It already exists.", branch.getName());
+            throw new BadRequest(String.format("Branch with name %s already exists.", branch.getName()));
         }
+        logger.info("Branch with name '{}' created.", branch.getName());
         return this.branchRepository.save(branch);
     }
 
@@ -34,12 +43,15 @@ public record BranchService(BranchRepository branchRepository) {
         Branch found = this.getById(branchId);
         found.setName(branch.getName());
         found.setZipCode(branch.getZipCode());
+        logger.info("Branch with id '{}' updated.", branchId);
         return this.branchRepository.save(found);
     }
 
     public void delete(String branchId) {
-        if (!this.branchRepository.existsById(branchId))
+        if (!this.branchRepository.existsById(branchId)) {
+            logger.warn("Could not delete branch with id '{}'. It does not exist.", branchId);
             throw new BadRequest(String.format("Branch with id %s does not exist", branchId));
+        }
         this.branchRepository.deleteById(branchId);
     }
 }
