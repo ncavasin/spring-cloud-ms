32a74f4b2363b8de3ec1f9a180e961222f2ef094
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-20 10:18:07 -0300

    update TopicDto to receive a set of movieIds as string

--- customer/src/main/java/com/ms/customer/topic/dto/TopicDto.java
+++ customer/src/main/java/com/ms/customer/topic/dto/TopicDto.java
@@ -2,6 +2,8 @@ package com.ms.customer.topic.dto;
 
 import lombok.Builder;
 
+import java.util.Set;
+
 @Builder
-public record TopicDto(String id, String name) {
+public record TopicDto(String id, String name, Set<String> movieIds) {
 }
