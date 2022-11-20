3300d5c8eab06c37e646366d08db7de6259c8e85
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-20 20:35:35 -0300

    add equals and hashcode to show

--- customer/src/main/java/com/ms/customer/show/entity/Show.java
+++ customer/src/main/java/com/ms/customer/show/entity/Show.java
@@ -11,6 +11,7 @@ import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import java.sql.Date;
 import java.sql.Time;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -22,8 +23,10 @@ public class Show extends TimeTrackable {
     @Column(nullable = false)
     protected Date date;
 
+    @Column(nullable = false)
     protected Time beginTime;
 
+    @Column(nullable = false)
     protected Time endTime;
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
@@ -31,4 +34,16 @@ public class Show extends TimeTrackable {
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     protected Room room;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Show show)) return false;
+        return getDate().equals(show.getDate()) && getBeginTime().equals(show.getBeginTime()) && getEndTime().equals(show.getEndTime()) && getMovie().equals(show.getMovie()) && getRoom().equals(show.getRoom());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDate(), getBeginTime(), getEndTime(), getMovie(), getRoom());
+    }
 }
