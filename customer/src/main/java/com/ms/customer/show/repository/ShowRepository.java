1d7bdf5f9407052ebd86e7d6745d4569cca28d36
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-20 21:20:31 -0300

    add query for finding by date begin and end times

--- customer/src/main/java/com/ms/customer/show/repository/ShowRepository.java
+++ customer/src/main/java/com/ms/customer/show/repository/ShowRepository.java
@@ -2,8 +2,10 @@ package com.ms.customer.show.repository;
 
 import com.ms.customer.show.entity.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.sql.Date;
+import java.sql.Time;
 import java.util.List;
 
 public interface ShowRepository extends JpaRepository<Show, String> {
@@ -11,4 +13,5 @@ public interface ShowRepository extends JpaRepository<Show, String> {
     List<Show> findAll();
 
     List<Show> findByDate(Date date);
+    List<Show> findByDateAndBeginTimeAndEndTime(Date date, Time beginTime, Time endTime);
 }
