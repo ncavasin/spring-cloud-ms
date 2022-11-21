4a8a72ebceb9c9a08e0b0abb1144ba1e01468ff3
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-21 03:54:09 -0300

    update topic to use naturalId + cache

--- customer/src/main/java/com/ms/customer/topic/entity/Topic.java
+++ customer/src/main/java/com/ms/customer/topic/entity/Topic.java
@@ -3,6 +3,9 @@ package com.ms.customer.topic.entity;
 import com.ms.customer.movie.entity.Movie;
 import com.ms.customer.shared.entities.TimeTrackable;
 import lombok.*;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
 
 import javax.persistence.*;
 import java.util.LinkedHashSet;
@@ -15,8 +18,11 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@NaturalIdCache
 public class Topic extends TimeTrackable {
     @Column(nullable = false, unique = true)
+    @NaturalId
     protected String name;
 
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
@@ -34,7 +40,7 @@ public class Topic extends TimeTrackable {
 
     @Override
     public int hashCode() {
-        return Objects.hash(getName(), getMovies());
+        return Objects.hash(getName());
     }
 
 }
