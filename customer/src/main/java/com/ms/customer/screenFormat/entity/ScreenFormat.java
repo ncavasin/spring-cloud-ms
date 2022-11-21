b74f3c3b7ef84e3ebbf7f90befa71a42eebfa705
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-20 21:29:40 -0300

    make screen width and height not nullable

--- customer/src/main/java/com/ms/customer/screenFormat/entity/ScreenFormat.java
+++ customer/src/main/java/com/ms/customer/screenFormat/entity/ScreenFormat.java
@@ -5,6 +5,7 @@ import com.ms.customer.shared.entities.TimeTrackable;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 @Getter
@@ -16,9 +17,25 @@ import java.util.Set;
 public class ScreenFormat extends TimeTrackable {
     @Column(nullable = false)
     protected String name;
+
+    @Column(nullable = false)
     protected Double screenHeight;
+
+    @Column(nullable = false)
     protected Double screenWidth;
 
     @OneToMany(mappedBy = "screenFormat", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     protected Set<Room> rooms;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ScreenFormat that)) return false;
+        return getName().equals(that.getName()) && getScreenHeight().equals(that.getScreenHeight()) && getScreenWidth().equals(that.getScreenWidth()) && getRooms().equals(that.getRooms());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getScreenHeight(), getScreenWidth(), getRooms());
+    }
 }
