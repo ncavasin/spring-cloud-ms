10e17431734de970fef0e72528d006eb1434957a
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-23 21:44:20 -0300

    add fake queries to compile

--- customer/src/main/java/com/ms/customer/seat/repository/SeatRepository.java
+++ customer/src/main/java/com/ms/customer/seat/repository/SeatRepository.java
@@ -2,11 +2,14 @@ package com.ms.customer.seat.repository;
 
 import com.ms.customer.seat.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SeatRepository extends JpaRepository<Seat, String> {
+    @Query(value = "SELECT s from Seat  s")
     List<Seat> findAvailable();
 
+    @Query(value = "SELECT s from Seat  s")
     List<Seat> findReserved();
 }
