69148d5eef4b8820459bee9180258b16a33bc721
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-26 02:17:41 -0300

    update migration to match new seat definition

--- customer/src/main/resources/db/migration/V2__ADD_SEAT_ENTITY.sql
+++ customer/src/main/resources/db/migration/V2__ADD_SEAT_ENTITY.sql
@@ -1,12 +1,15 @@
 CREATE TABLE IF NOT EXISTS seat
 (
-    id                 VARCHAR(255) NOT NULL,
-    creation_timestamp TIMESTAMP WITHOUT TIME ZONE,
-    update_timestamp   TIMESTAMP WITHOUT TIME ZONE,
-    seat_row           VARCHAR(255) NOT NULL,
-    seat_column        INTEGER      NOT NULL,
-    reserved           BOOLEAN      NOT NULL,
-    CONSTRAINT pk_seat PRIMARY KEY (id),
-    CONSTRAINT unique_seat_natural_id UNIQUE (seat_row, seat_column)
+    id                   VARCHAR(255) NOT NULL,
+    creation_timestamp   TIMESTAMP WITHOUT TIME ZONE,
+    update_timestamp     TIMESTAMP WITHOUT TIME ZONE,
+    seat_row             VARCHAR(255) NOT NULL,
+    seat_column          INTEGER      NOT NULL,
+    room_id              VARCHAR(255) NOT NULL,
+    confirmation         TIMESTAMP WITHOUT TIME ZONE,
+    selection            TIMESTAMP WITHOUT TIME ZONE,
+    selection_expiration TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT pk_seat PRIMARY KEY (id, seat_row, seat_column),
+    CONSTRAINT fk_room_seat FOREIGN KEY (room_id) REFERENCES room (id)
 );
 
