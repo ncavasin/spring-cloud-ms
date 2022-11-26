2d7d3572c57cd060f562fd70db5fab5368b887dc
Nicolas Cavasin <ncavasin97@gmail.com>
2022-11-26 02:18:00 -0300

    replace Set by List

--- customer/src/main/java/com/ms/customer/seat/service/SeatService.java
+++ customer/src/main/java/com/ms/customer/seat/service/SeatService.java
@@ -3,20 +3,23 @@ package com.ms.customer.seat.service;
 import com.ms.customer.seat.entity.Seat;
 import com.ms.customer.seat.entity.dto.SeatDto;
 
+import java.util.List;
 import java.util.Set;
 
 public interface SeatService {
-    Set<Seat> findAll();
+    List<Seat> findAll();
 
-    Set<Seat> findAvailable();
+    List<Seat> findNotSelected();
 
-    Set<Seat> findReserved();
+    List<Seat> findReserved();
 
     Seat findById(String id);
 
     Seat add(SeatDto seatDto);
 
-    Seat update(String id, SeatDto seatDto);
+    List<Seat> select(Set<SeatDto> seatDtos);
+
+    List<Seat> confirm(Set<SeatDto> seatDtos);
 
     void delete(String id);
 }
