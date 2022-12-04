b8d72d8ba08bfa811aed25dc6d8c1ba98d91a376
Nicolas Cavasin <ncavasin97@gmail.com>
2022-12-04 19:29:28 -0300

    add seat entity

--- customer/src/main/resources/db/migration/V2__ADD_SEAT_ENTITY.sql
+++ customer/src/main/resources/db/migration/V2__ADD_SEAT_ENTITY.sql
@@ -6,10 +6,8 @@ CREATE TABLE IF NOT EXISTS seat
     seat_row             VARCHAR(255) NOT NULL,
     seat_column          INTEGER      NOT NULL,
     room_id              VARCHAR(255) NOT NULL,
-    confirmation         TIMESTAMP WITHOUT TIME ZONE,
-    selection            TIMESTAMP WITHOUT TIME ZONE,
-    selection_expiration TIMESTAMP WITHOUT TIME ZONE,
     CONSTRAINT pk_seat PRIMARY KEY (id, seat_row, seat_column),
+    CONSTRAINT unique_seat_natural_id UNIQUE (seat_row, seat_column),
     CONSTRAINT fk_room_seat FOREIGN KEY (room_id) REFERENCES room (id)
 );
 
