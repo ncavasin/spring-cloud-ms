54e6a9567371155321c3670ce3d76f015d3d5bc4
Nicolas Cavasin <ncavasin97@gmail.com>
2022-12-26 19:25:07 -0300

    remove unused imports

--- customer/src/main/java/com/ms/customer/migration/StartUpMigration.java
+++ customer/src/main/java/com/ms/customer/migration/StartUpMigration.java
@@ -1,7 +1,6 @@
 package com.ms.customer.migration;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface StartUpMigration {
     void run(ApplicationReadyEvent applicationReadyEvent);
