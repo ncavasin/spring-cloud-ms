2f5410a6fd69bf4bc002d7b5d6766a978bdc5e18
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-01 14:30:48 -0300

    add show existence query

--- customer/src/main/java/com/ms/customer/show/repository/ShowRepository.java
+++ customer/src/main/java/com/ms/customer/show/repository/ShowRepository.java
@@ -6,12 +6,18 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public interface ShowRepository extends JpaRepository<Show, String> {
+
+    @Query("select (count(s) > 0) from Show s where s.room.id = ?1 and s.date = ?2 and s.beginTime = ?3 and s.endTime = ?4")
+    boolean existsByRoomIdAndDateAndBeginTimeAndEndTime(String id, Date date, ZonedDateTime beginTime, ZonedDateTime endTime);
+
     @Override
     List<Show> findAll();
 
     List<Show> findByDate(Date date);
+
     List<Show> findByDateAndBeginTimeAndEndTime(Date date, Time beginTime, Time endTime);
 }
