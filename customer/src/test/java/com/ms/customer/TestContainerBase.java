6659533ef9c9ab456e13d8393445a3550fe3bbb8
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-02 23:03:00 -0300

    leave fossil in base testcontainer just in case

--- customer/src/test/java/com/ms/customer/TestContainerBase.java
+++ customer/src/test/java/com/ms/customer/TestContainerBase.java
@@ -2,6 +2,8 @@ package com.ms.customer;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -9,6 +11,19 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 @ActiveProfiles("test")
 public abstract class TestContainerBase {
-    private static PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:12.13-alpine");
+//    private static final PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:14.2-alpine");
 
+    private static final PostgreSQLContainer POSTGRES_SQL_CONTAINER;
+
+    static {
+        POSTGRES_SQL_CONTAINER = new PostgreSQLContainer<>("postgres:14.2-alpine");
+        POSTGRES_SQL_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void overrideTestProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRES_SQL_CONTAINER::getPassword);
+    }
 }
