1cc846caa4ea4a113c688fa5fbd9f6755a7067ae
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-02 21:14:21 -0300

    add commented test classes

--- customer/src/test/java/com/ms/customer/CustomerApplicationTests.java
+++ customer/src/test/java/com/ms/customer/CustomerApplicationTests.java
@@ -1,16 +1,17 @@
-package com.ms.customer;
-
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-//@ActiveProfiles("mem")
-@SpringBootTest
-public class CustomerApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-}
+//package com.ms.customer;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@ActiveProfiles("test")
+//@SpringBootTest
+//public class CustomerApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//}
