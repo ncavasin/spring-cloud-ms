a2acd19301ddf902946a611674af4b2be4c33604
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-03 00:17:11 -0300

    add alternative as fossil

--- customer/src/test/java/com/ms/customer/TestContainerBase.java
+++ customer/src/test/java/com/ms/customer/TestContainerBase.java
@@ -12,36 +12,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @ActiveProfiles("test")
 public abstract class TestContainerBase {
 //    private static final PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:14.2-alpine");
-
-//    import org.testcontainers.containers.PostgreSQLContainer;
-//import org.testcontainers.junit.jupiter.Container;
-//import org.testcontainers.junit.jupiter.Testcontainers;
-//
-//@Testcontainers
-//public abstract class AbstractPostgreSQLTest {
-//
-//    @Container
-//    private static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:11.5")
-//        .withDatabaseName("mydatabase")
-//        .withUsername("user")
-//        .withPassword("password");
-//
-//    protected PostgreSQLContainer getPostgreSQLContainer() {
-//        return postgres;
-//    }
-//}
-//
-//public class TestcontainerTest extends AbstractPostgreSQLTest {
-//
-//    @Test
-//    public void test() {
-//        // your test code goes here
-//        PostgreSQLContainer postgres = getPostgreSQLContainer();
-//        // use postgres in your test code
-//    }
-//}
-
-    private static final PostgreSQLContainer POSTGRES_SQL_CONTAINER;
+        private static final PostgreSQLContainer POSTGRES_SQL_CONTAINER;
 
     static {
         POSTGRES_SQL_CONTAINER = new PostgreSQLContainer<>("postgres:14.2-alpine");
@@ -55,3 +26,35 @@ public abstract class TestContainerBase {
         registry.add("spring.datasource.password", POSTGRES_SQL_CONTAINER::getPassword);
     }
 }
+
+/*
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractPostgreSQLTest {
+
+    @Container
+    private static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:11.5")
+        .withDatabaseName("mydatabase")
+        .withUsername("user")
+        .withPassword("password");
+
+    protected PostgreSQLContainer getPostgreSQLContainer() {
+        return postgres;
+    }
+}
+
+public class TestcontainerTest extends AbstractPostgreSQLTest {
+
+    @Test
+    public void test() {
+        // your test code goes here
+        PostgreSQLContainer postgres = getPostgreSQLContainer();
+        // use postgres in your test code
+    }
+}
+
+
+ */
