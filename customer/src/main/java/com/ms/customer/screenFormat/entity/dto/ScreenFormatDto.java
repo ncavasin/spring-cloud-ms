f8acdb36df989fff96e14a2dca1d91871206c02a
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-04 21:49:54 -0300

    replace not empty with not null for numbers

--- customer/src/main/java/com/ms/customer/screenFormat/entity/dto/ScreenFormatDto.java
+++ customer/src/main/java/com/ms/customer/screenFormat/entity/dto/ScreenFormatDto.java
@@ -3,13 +3,14 @@ package com.ms.customer.screenFormat.entity.dto;
 import lombok.Builder;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.Set;
 
 @Builder
 public record ScreenFormatDto(String id,
                               @NotEmpty String name,
-                              @NotEmpty @Positive Double screenHeight,
-                              @NotEmpty @Positive Double screenWidth,
+                              @NotNull @Positive Double screenHeight,
+                              @NotNull @Positive Double screenWidth,
                               Set<String> roomIds) {
 }
