ef47d5abb7d850aefe2f4ce738121657db467676
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-04 21:37:42 -0300

    add validation checks

--- customer/src/main/java/com/ms/customer/screenFormat/entity/dto/ScreenFormatDto.java
+++ customer/src/main/java/com/ms/customer/screenFormat/entity/dto/ScreenFormatDto.java
@@ -2,8 +2,14 @@ package com.ms.customer.screenFormat.entity.dto;
 
 import lombok.Builder;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
 import java.util.Set;
 
 @Builder
-public record ScreenFormatDto(String id, String name, Double screenHeight, Double screenWidth, Set<String> roomIds) {
+public record ScreenFormatDto(String id,
+                              @NotEmpty String name,
+                              @NotEmpty @Positive Double screenHeight,
+                              @NotEmpty @Positive Double screenWidth,
+                              Set<String> roomIds) {
 }
