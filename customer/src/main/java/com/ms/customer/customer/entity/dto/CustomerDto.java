216f39f9d3a04e205327a224bee37af2ff787258
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-04 21:29:59 -0300

    add spring validation dep

--- customer/src/main/java/com/ms/customer/customer/entity/dto/CustomerDto.java
+++ customer/src/main/java/com/ms/customer/customer/entity/dto/CustomerDto.java
@@ -2,6 +2,12 @@ package com.ms.customer.customer.entity.dto;
 
 import lombok.Builder;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Builder
-public record CustomerDto(String id, String email, String password) {
+public record CustomerDto(String id,
+                          @Email @NotEmpty String email,
+                          @Size(min = 8, message = "Minimum password length is 8 characters") @NotEmpty String password) {
 }
