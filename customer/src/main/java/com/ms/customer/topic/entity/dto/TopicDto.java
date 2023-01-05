b8fa091f48ed346a7914d628b4f8bb03cd1c5fd8
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-04 22:25:35 -0300

    add dto validation

--- customer/src/main/java/com/ms/customer/topic/entity/dto/TopicDto.java
+++ customer/src/main/java/com/ms/customer/topic/entity/dto/TopicDto.java
@@ -1,9 +1,13 @@
 package com.ms.customer.topic.entity.dto;
 
 import lombok.Builder;
+import org.springframework.lang.Nullable;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.Set;
 
 @Builder
-public record TopicDto(String id, String name, Set<String> movieIds) {
+public record TopicDto(String id,
+                       @NotEmpty String name,
+                       @Nullable Set<String> movieIds) {
 }
