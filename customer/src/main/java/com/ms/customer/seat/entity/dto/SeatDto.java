de28eb4256606d15f93d7f2a50e2adc67e3fd996
Nicolas Cavasin <ncavasin97@gmail.com>
2023-01-08 00:11:21 -0300

    add restrictions

--- customer/src/main/java/com/ms/customer/seat/entity/dto/SeatDto.java
+++ customer/src/main/java/com/ms/customer/seat/entity/dto/SeatDto.java
@@ -3,6 +3,11 @@ package com.ms.customer.seat.entity.dto;
 import com.ms.customer.seat.entity.SeatNaturalId;
 import lombok.Builder;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Builder
-public record SeatDto(String id, SeatNaturalId seatNaturalId, String roomId) {
+public record SeatDto(String id,
+                      @NotNull SeatNaturalId seatNaturalId,
+                      @NotEmpty String roomId) {
 }
--- customer/src/main/java/com/ms/customer/seat/entity/dto/SeatNaturalIdDto.java
+++ customer/src/main/java/com/ms/customer/seat/entity/dto/SeatNaturalIdDto.java
@@ -2,6 +2,12 @@ package com.ms.customer.seat.entity.dto;
 
 import lombok.Builder;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Builder
-public record SeatNaturalIdDto(String seatRow, Integer seatColumn) {
+public record SeatNaturalIdDto(String id,
+                               @NotEmpty String seatRow,
+                               @NotNull @Positive(message = "Columns begin at number 1") Integer seatColumn) {
 }
